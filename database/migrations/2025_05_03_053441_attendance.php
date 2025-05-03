8806c6706db0d3471603e74497a27a0f80898e60
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2025-05-03 13:49:14 +0800

    migration

---
+++ database/migrations/2025_05_03_053441_attendance.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance', function(Blueprint $table){
+            $table->id();
+            $table->string('uuid',15)->unique();
+            $table->string('attID', 15)->nullable();
+            $table->string('time_in', 35)->nullable();
+            $table->string('time_out', 35)->nullable();
+            $table->string('date', 30)->nullable();
+            $table->boolean('is_deleted');
+            $table->string('created_at');
+            $table->string('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance');
+    }
+};
