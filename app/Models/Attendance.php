b5333eb512b6b9e001233a86e0fa7f2eb24d5330
CedrickAlegsao <cedrickcuencaalegsao@gmail.com>
2025-10-29 17:33:08 +0800

    update

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class attendance extends Model
+class Attendance extends Model
 {
     protected $table = 'attendance';
     protected $fillable = [
